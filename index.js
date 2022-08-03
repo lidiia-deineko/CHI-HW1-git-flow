9991efb378ff619467d1ef5f056a4864f794b7d9
Lida Deineko <93deineko@gmail.com>
2022-08-03 12:53:33 +0300

    Change in feature_2 function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log('Develop branch');
 
 const feature_2 = () => {
-    console.log('It`s a feature_2 function')
+    console.log('It`s a feature_2 function');
+    console.log('This is a change in feature_2 function');
 }
